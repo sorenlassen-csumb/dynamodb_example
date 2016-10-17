158c7bd816a9f429ca61b9e9fe8678ac50ed2a20
Soren Lassen <soren@fb.com>
2016-10-16 19:54:43 -0700

    Removed code duplication between Executor and RestExecutor.
    Test plan:
    Ran
    mvn exec:java -Dexec.mainClass=edu.csumb.dynamodbexample.Executor
    and inspected output.
    Ran
    mvn jetty:run
    and opened http://localhost:8080/, clicked Run Command, and inspected output.

--- src/main/java/edu/csumb/dynamodbexample/Executor.java
+++ src/main/java/edu/csumb/dynamodbexample/Executor.java
@@ -1,25 +1,21 @@
 package edu.csumb.dynamodbexample;
 
 import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
+import java.io.PrintStream;
 
 /**
  * Created by ndavidson on 9/21/16.
  */
 
 public class Executor {
-    public static DBController controller;
-    public static void main(String[] args){
-        controller = new DBController(); // connect to the dynamodb server
+    public static void execute(PrintStream out){
+        DBController controller = new DBController(); // connect to the dynamodb server
         if (CreateExamples.createTable("test", controller)) {
-            System.out.println("Table Created!");
+            out.println("Table Created!");
         } else {
-            System.out.println("Table Already Exists!");
+            out.println("Table Already Exists!");
         }
 
     /*
@@ -33,41 +29,44 @@ public class Executor {
     */
 
         if (CreateExamples.createRow("test", 1, "ping", controller)) { // create row in tablename = test, with id=1 and dataToStore=ping
-            System.out.println("Created row!");
+            out.println("Created row!");
         } else {
-            System.out.println("Failed to create row!");
+            out.println("Failed to create row!");
         }
 
         Item item = SelectExamples.get("test", 1, controller); // fetch from the table "test" with the id = 1
 
-        System.out.println("Data from db: " + item.get("dataStored")); // print out ping
+        out.println("Data from db: " + item.get("dataStored")); // print out ping
 
         // Update row!
 
         if (UpdateExamples.update("test", 1, "pong", controller)) {
-            System.out.println("Updated Row!");
+            out.println("Updated Row!");
 
             item = SelectExamples.get("test", 1, controller); // read from the DB again to check the value of dataStored
-            System.out.println("New data value from db: " + item.get("dataStored"));
+            out.println("New data value from db: " + item.get("dataStored"));
         } else {
-            System.out.println("Failed to update row!");
+            out.println("Failed to update row!");
         }
 
 
         if (DeleteExamples.delete("test", 1, controller)) { // Now delete the entry
-            System.out.println("Deleted row!");
+            out.println("Deleted row!");
         } else {
-            System.out.println("Failed to delete row!");
+            out.println("Failed to delete row!");
         }
 
         item = SelectExamples.get("test", 1, controller);
         if (item == null) {
-            System.out.println("Row does not exist!");
+            out.println("Row does not exist!");
         } else {
-            System.out.println("Woops, row was not deleted!");
+            out.println("Woops, row was not deleted!");
         }
 
         controller.close();
     }
 
+    public static void main(String[] args){
+        execute(System.out);
+    }
 }
--- src/main/java/edu/csumb/dynamodbexample/rest/RestExecutor.java
+++ src/main/java/edu/csumb/dynamodbexample/rest/RestExecutor.java
@@ -1,12 +1,13 @@
 package edu.csumb.dynamodbexample.rest;
 
 import com.amazonaws.services.dynamodbv2.document.Item;
-import edu.csumb.dynamodbexample.*;
+import edu.csumb.dynamodbexample.Executor;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 /**
  * Created by nathanieldavidson on 9/27/16.
@@ -14,65 +15,13 @@ import java.io.IOException;
 
 @Path("/")
 public class RestExecutor {
-    public DBController controller;
     @GET
     @Path("/executor")
     public Response index(){
-        String html = "";
-        controller = new DBController(); // connect to the dynamodb server
-        if (CreateExamples.createTable("test", controller)) {
-            html += "Table Created!<br/>";
-        } else {
-            html += "Table Already Exists!<br/>";
-        }
-
-    /*
-                                         _
-                                        | |
-                      ___ _ __ _   _  __| |
-                     / __| '__| | | |/ _` |
-                    | (__| |  | |_| | (_| |
-                     \___|_|   \__,_|\__,_|
-
-    */
-
-        if (CreateExamples.createRow("test", 1, "ping", controller)) { // create row in tablename = test, with id=1 and dataToStore=ping
-            html += "Created row!<br/>";
-        } else {
-            html += "Failed to create row!<br/>";
-        }
-
-        Item item = SelectExamples.get("test", 1, controller); // fetch from the table "test" with the id = 1
-
-        html += "Data from db: " + item.get("dataStored") + "<br/>"; // print out ping
-
-        // Update row!
-
-        if (UpdateExamples.update("test", 1, "pong", controller)) {
-            html += "Updated Row!<br/>";
-
-            item = SelectExamples.get("test", 1, controller); // read from the DB again to check the value of dataStored
-            html += "New data value from db: " + item.get("dataStored") + "<br/>";
-        } else {
-            html += "Failed to update row!<br/>";
-        }
-
-
-        if (DeleteExamples.delete("test", 1, controller)) { // Now delete the entry
-            html += "Deleted row!<br/>";
-        } else {
-            html += "Failed to delete row!<br/>";
-        }
-
-        item = SelectExamples.get("test", 1, controller);
-        if (item == null) {
-            html += "Row does not exist!<br/>";
-        } else {
-            html += "Woops, row was not deleted!<br/>";
-        }
-
-        controller.close();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Executor.execute(new PrintStream(out));
+        String lineSeparator = System.getProperty("line.separator", "\n");
+        String html = out.toString().replace(lineSeparator, "<br/>\n");
         return Response.ok(html).build();
     }
-
 }
